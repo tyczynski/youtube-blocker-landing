80667225458f5fe4c3490836dd31114e1c07ba6f
Przemysław Tyczyński <przemyslaw@tyczynski.pl>
2020-10-27 22:12:14 +0100

    feat: create `ContainerSlim` component with smaller with

--- components/Container.ts
+++ components/Container.ts
@@ -1,14 +1,21 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-  max-width: 1200px;
+const ContainerBase = styled.div`
   padding-left: 3.2rem;
   padding-right: 3.2rem;
   margin-left: auto;
   margin-right: auto;
 
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: 992px) {
     padding-left: 2rem;
     padding-right: 2rem;
   }
+`
+
+export const Container = styled(ContainerBase)`
+  max-width: 1200px;
+`
+
+export const ContainerSlim = styled(ContainerBase)`
+  max-width: 1072px;
 `
