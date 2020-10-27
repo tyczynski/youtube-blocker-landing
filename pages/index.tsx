42a450e817d02f6c2f241daf246fc7a5ef872b18
Przemysław Tyczyński <przemyslaw@tyczynski.pl>
2020-10-27 22:18:52 +0100

    feat: add meta title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next'
+import Head from 'next/head'
 import { Navbar } from '@components/Navbar'
 import { Header } from '@components/Header'
 import { Features } from '@components/Features'
@@ -6,6 +7,11 @@ import { Footer } from '@components/Footer'
 
 const HomePage: NextPage = () => (
   <>
+    <Head>
+      <title>
+        YouTube Blocker - Easy way to block channels that you don't want to see
+      </title>
+    </Head>
     <Navbar />
     <Header />
     <Features />
