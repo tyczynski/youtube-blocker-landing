136f9624fc0ba9d2429882e3b9644ffc404a1ad1
Przemysław Tyczyński <przemyslaw@tyczynski.pl>
2020-10-27 17:22:19 +0100

    feat: Container component

---
+++ components/Container.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  max-width: 1200px;
+  padding-left: 3.2rem;
+  padding-right: 3.2rem;
+  margin-left: auto;
+  margin-right: auto;
+
+  @media screen and (min-width: 768px) {
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+`
