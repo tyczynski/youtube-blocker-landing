b6c425ecc487f782cd4b6bf8e23872211988d28e
Przemysław Tyczyński <przemyslaw@tyczynski.pl>
2020-10-27 22:13:07 +0100

    feat: use `Features` component in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import { NextPage } from 'next'
 import { Navbar } from '@components/Navbar'
 import { Header } from '@components/Header'
+import { Features } from '@components/Features'
 import { Footer } from '@components/Footer'
 
 const HomePage: NextPage = () => (
   <>
     <Navbar />
     <Header />
+    <Features />
     <Footer />
   </>
 )
